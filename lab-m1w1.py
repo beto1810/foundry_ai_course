bf7bebf20e57830c5d21616844367b094fb7781e
Dat_Nguyen <dat.nguyen.1810@gmail.com>
2025-03-29 12:14:31 +0700

    finish lab1 lessons

---
+++ lab-m1w1.py
@@ -0,0 +1,78 @@
+import requests
+import pandas as pd
+import duckdb 
+
+def extract_data():
+    """ Extract data from the API and save it as a Parquet file """
+
+    url = "https://pokeapi.co/api/v2/pokemon"  # Start URL
+    all_pokemon = []
+
+    response = requests.get(url, timeout=10)
+
+    data = response.json()
+    pokemon_list = [pokemon["name"] for pokemon in data["results"]]
+    all_pokemon.extend(pokemon_list)
+
+    print(f"Extracted {len(all_pokemon)} Pokémon names from the API.")
+
+    # Loop through the Pokémon names and fetch their details
+    df = pd.DataFrame(all_pokemon)
+
+    # Save as Parquet
+    parquet_filename = "pokemon_data.parquet"
+    df.to_parquet(parquet_filename, engine="pyarrow")
+
+    print(f"Saved {len(df)} Pokémon to {parquet_filename}")
+
+
+    return all_pokemon
+
+
+def load_data():
+    con = duckdb.connect("pokemon.db")
+    # Path to Parquet file
+    parquet_file = "pokemon_data.parquet"
+
+    # Create or replace the table
+    con.execute("DROP TABLE IF EXISTS pokedex")  
+    con.execute("""
+        CREATE OR REPLACE TABLE pokedex AS 
+        SELECT 
+            row_number() OVER () AS id,  -- Auto-increment `id`
+            *  -- Select all other columns from the Parquet file
+        FROM read_parquet(?)
+    """, [parquet_file])
+
+    con.close()
+
+
+def transform_pokemon_data():
+    con = duckdb.connect("pokemon.db")
+    con.execute("""
+        CREATE OR REPLACE TABLE pokemon_stats AS 
+        SELECT COUNT(ID) AS total_pokemon, 
+            MIN(ID) AS first_id_pokemon, 
+            MAX(ID) AS last_id_pokemon
+        FROM pokedex
+    """)
+
+    result_1 = con.execute("SELECT * FROM pokedex").fetchdf()
+
+    result = con.execute("SELECT * FROM pokemon_stats").fetchdf()
+
+    con.close()
+
+    print(result)
+    print(result_1)
+
+
+    
+
+def main():
+    load_data()
+    transform_pokemon_data()
+
+if __name__ == "__main__":
+    main()
+
